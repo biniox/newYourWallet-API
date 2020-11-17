24a3962483f6f4b42a3fa291aa52cf48b1fa82c5
Jarek <biniox2@gmail.com>
2020-11-17 14:45:15 +0100

    init

---
+++ db/client.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://root:root@cluster0.pputj.mongodb.net/wallet?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log("Database connection is established"));
+
+  module.exports = db;
